ac4566326c6abe7ef7e4d23a7a2f8ca8bae5f351
Antonio Maiorano <amaiorano@gmail.com>
2016-10-31 23:18:40 -0400

    Guard MFCWindow.h/cpp with GS_MFC_ENABLED macro

--- source/gsgamelib/src/gs/Platform/Win32/MFCWindow.cpp
+++ source/gsgamelib/src/gs/Platform/Win32/MFCWindow.cpp
@@ -1,7 +1,11 @@
 #include "MFCWindow.h"
+#ifdef GS_MFC_ENABLED
+
 #include <cassert>
 
 void MFCWindow::Create(const char* /*title*/, int /*width*/, int /*height*/, int /*bpp*/, bool /*bFullScreen*/)
 {
 	assert(m_hWnd && m_hInstance && "Make sure to call SetHandles() before the window is created!");
 }
+
+#endif // GS_MFC_ENABLED
